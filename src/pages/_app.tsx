c9e45322fc34d798a1acdb64dfb7d685e90b51ba
leofuna <leo.funa@gmail.com>
2023-04-27 18:32:11 -0300

    feat: protecting pages on application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,32 @@
-// import '@/styles/globals.css';
+import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider, signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+
+type AuthTypes = {
+  children : React.ReactElement;
+}
+
+const Auth = ({ children }: AuthTypes) => {
+  const { status } = useSession();
+  const router = useRouter();
+  if (status === 'unauthenticated' && router.pathname !== '/signin') {
+    signIn();
+  }
+  if (status === 'loading' && router.pathname !== '/signin') {
+    return <div>Verificando token...</div>
+  }
+  return (children)
+}
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      {Component.isProtected
+      ?  <Auth><Component {...pageProps} /></Auth>
+      : <Component {...pageProps} />
+    }
     </SessionProvider>
   )
 };
