f765c800c3ba9216510065afe38775311dcd27a9
leofuna <leo.funa@gmail.com>
2023-05-02 15:30:36 -0300

    style: user details page

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,9 +1,14 @@
 import { useSession } from "next-auth/react"
+import { Roboto } from "next/font/google";
+import Head from "next/head";
 import Image from "next/image"
 import { useRouter } from "next/router";
 
+const inter = Roboto({ weight: '400', subsets: ['latin'] })
+
 const styles = {
-  span: 'font-bold'
+  span: 'font-bold text-purple-600',
+  input: 'text-lg text-slate-600',
 }
 
 const getBrowserName = (userAgent: string): string => {
@@ -18,25 +23,32 @@ function Users() {
   const router = useRouter();
   const userAgent = navigator.userAgent;
 
-  if(!session?.user) return <main>Você não está logado, favor efetuar login...</main>
+  if (!session?.user) return <main className={`flex text-5xl text-white items-center justify-center h-screen w-screen bg-gradient-to-br from-cyan-300 to-sky-600 ${inter.className}`}>Página protegida, Redirecionando...</main>
   return (
     <>
-      <header className="flex justify-center">
-        <button className="border w-40 border-black mt-4 hover:bg-slate-400" onClick={() => router.push('/')}>
-          Início
-        </button>
-      </header>
-      <main className='flex flex-col justify-center items-center h-screen'>
-        <Image src={session?.user?.image || ''} width={200} height={200} className="rounded-full mb-6" alt="Avatar image" />
-        <h3><span className={styles.span}>Nome: </span>{session?.user?.name}</h3>
-        <h3><span className={styles.span}>Email: </span>{session?.user?.email}</h3>
-        <h3><span className={styles.span}>Navegador: </span>{getBrowserName(userAgent)}</h3>
-        <h3><span className={styles.span}>Logado desde: </span>{session?.loginTime}</h3>
-      </main>
+      <Head><title>Detalhes</title></Head>
+      <div className={`flex items-center justify-center h-screen w-screen bg-gradient-to-br from-cyan-300 to-sky-600 ${inter.className}`}>
+        <main className='bg-white rounded-lg flex flex-col items-center justify-center w-2/6 py-10 gap-2 shadow-2xl'>
+          <Image src={session?.user?.image || ''} width={200} height={200} className="rounded-full mb-6 shadow-xl border border-purple-600" alt="Avatar image" />
+          <h3 className={styles.input}><span className={styles.span}>Nome: </span>{session?.user?.name}</h3>
+          <h3 className={styles.input}><span className={styles.span}>Email: </span>{session?.user?.email}</h3>
+          <h3 className={styles.input}><span className={styles.span}>Navegador: </span>{getBrowserName(userAgent)}</h3>
+          <h3 className={styles.input}><span className={styles.span}>Logado desde: </span>{session?.loginTime}</h3>
+          <footer className="w-full flex justify-center">
+            <button
+              className="p-1 rounded-md border bg-gradient-to-tr from-purple-500 to-purple-600 text-white font-bold text-lg w-6/12 mt-10"
+              onClick={() => router.push('/')}
+            >
+              Início
+            </button>
+          </footer>
+        </main>
+      </div>
     </>
   )
 }
 
+// Aqui estamos protegende a página de usuários nao autenticados, em conjunto com o que foi feito em _app.tsx
 Users.isProtected = true;
 
 export default Users;
