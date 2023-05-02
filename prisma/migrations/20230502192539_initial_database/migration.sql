09e8bb07dc3372ddcf2c70c9d5fb162518d6c135
leofuna <leo.funa@gmail.com>
2023-05-02 16:57:21 -0300

    chore: prisma mysql schema

---
+++ prisma/migrations/20230502192539_initial_database/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `displayName` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Avatar` (
+    `id` VARCHAR(191) NOT NULL,
+    `avatarUrl` LONGTEXT NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
