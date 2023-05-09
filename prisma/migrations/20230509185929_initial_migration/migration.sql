837dc460bbe3ebbca8f6558c9d7c2f3fea2b9eb4
leofuna <leo.funa@gmail.com>
2023-05-09 16:07:54 -0300

    chore: removing foreign keys cause planetscale

--- prisma/migrations/20230509185929_initial_migration/migration.sql
+++ prisma/migrations/20230509185929_initial_migration/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE `User` (
     `id` VARCHAR(191) NOT NULL,
     `displayName` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     UNIQUE INDEX `User_email_key`(`email`),
@@ -13,6 +14,8 @@ CREATE TABLE `User` (
 CREATE TABLE `Avatar` (
     `id` VARCHAR(191) NOT NULL,
     `avatarUrl` LONGTEXT NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
 
+    UNIQUE INDEX `Avatar_userId_key`(`userId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
