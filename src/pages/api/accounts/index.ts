993012dec62f7746fe9f936769fa8ec36d814bda
leofuna <leo.funa@gmail.com>
2023-05-09 18:13:11 -0300

    fix: importing db again

--- src/pages/api/accounts/index.ts
+++ src/pages/api/accounts/index.ts
@@ -1,4 +1,4 @@
-// import db from './../../../../prisma/db';
+import db from './../../../../prisma/db';
 import { NextApiRequest, NextApiResponse } from 'next';
 import Cors from 'cors';
 import bcrypt from 'bcrypt';
