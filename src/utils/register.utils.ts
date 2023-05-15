ab4bc87b03b538e824cfb1460a41a342a9a49eaa
leofuna <leo.funa@gmail.com>
2023-05-15 17:27:56 -0300

    fix: validate max avatar image size

--- src/utils/register.utils.ts
+++ src/utils/register.utils.ts
@@ -6,9 +6,12 @@ type RegisterValidationsProps = {
   retypePassword: string;
 }
 
+const MAX_FILE_SIZE = 3500000;
+
 export const getMsgOnValidateError = ({ fullName, avatar, email, password, retypePassword }: RegisterValidationsProps): String => {
   if (fullName.trim().length < 3) return 'Nome deve ter pelo menos 4 caracteres!';
   if (!avatar) return 'Necessário um avatar';
+  if (avatar.size > MAX_FILE_SIZE) return 'Foto deve ter até 3.5MB'
   if (!email.trim()) return 'Email deve ser preenchido com algo!';
   if (!password.trim()) return 'Favor informar a senha...';
   if (password !== retypePassword) return 'Senhas não são idênticas!';
